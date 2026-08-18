e45aa9b95763cb4f8b6cebd2d6c8f16a2eeb9d44
Sreejith.k <sreejith.k@bytestrone.com>
2026-08-18 15:37:38 +0530

    Use dj_database_url.parse for explicit database url configuration

--- backend/docmind_backend/settings.py
+++ backend/docmind_backend/settings.py
@@ -96,8 +96,8 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 
 if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.config(
-            default=DATABASE_URL,
+        'default': dj_database_url.parse(
+            DATABASE_URL,
             conn_max_age=600,
             ssl_require=True
         )
